144d83bfa62652e5fb97435bbd2d600c23ec8fb0
Dominick Hera <dominickhera@me.com>
2018-10-12 18:27:13 -0400

    Initial Commit

---
+++ finnestFinalWorkSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finnestFinalWorkSample
+//
+//  Created by Dominick Hera on 10/12/18.
+//  Copyright © 2018 Dominick Hera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
